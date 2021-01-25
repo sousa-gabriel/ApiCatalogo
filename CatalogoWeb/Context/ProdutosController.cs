1a121bee62d9dc3325ed916b5f71f1d195537bdc
sousa-gabriel <sousa-gabriel@hotmail.com>
2021-01-25 17:00:11 -0300

    Alterando Padrão do Projeto para  DotNet 5.0

---
+++ CatalogoWeb/Context/ProdutosController.cs
@@ -0,0 +1,27 @@
+﻿using CatalogoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogoWeb.Context
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+           return _context.Produtos.ToList();
+        }
+    }
+}
