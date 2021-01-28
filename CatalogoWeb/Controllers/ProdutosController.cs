660939f1c1a5877dbdb620f5f157a50d9394b234
sousa-gabriel <sousa-gabriel@hotmail.com>
2021-01-27 23:20:56 -0300

    Adicionando os metodos Post/Put/ Delete na API

--- CatalogoWeb/Controllers/ProdutosController.cs
+++ CatalogoWeb/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using CatalogoWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace CatalogoWeb.Context
 {
     [Route("api/[Controller]")]
     [ApiController]
-    
+
     public class ProdutosController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -21,18 +22,52 @@ namespace CatalogoWeb.Context
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-           return _context.Produtos.ToList();
+            return _context.Produtos.ToList();
         }
-        
-        [HttpGet("{id}")]
+
+        [HttpGet("{id}", Name = "ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos.FirstOrDefault(P => P.ProdutoId == id);
-            if(produto == null)
+            if (produto == null)
             {
                 return NotFound();
             }
             return produto;
         }
+        [HttpPost]
+        public ActionResult Post([FromBody] Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId });
+
+        }
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Produto produto)
+        {
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Produto> Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok();
+
+        }
+
     }
 }
